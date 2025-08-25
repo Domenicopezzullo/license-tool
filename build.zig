2fa143027f647874c8c9fe4821b1d700b873d94e
Domenicopezzullo <pezzullodomenico712@gmail.com>
2025-08-25 14:41:17 +0000

    Init

---
+++ build.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const exe = b.addExecutable(.{
+        .name = "license",
+        .root_source_file = .{ .cwd_relative = "src/main.zig" },
+        .optimize = .ReleaseSmall,
+        .target = b.graph.host,
+    });
+    b.installArtifact(exe);
+}
